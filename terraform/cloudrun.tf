d58aa982f5baa4849d4ad0d70fdc5858224ba212
Your Name <mawardsantika@gmail.com>
2024-12-05 01:55:00 +0700

    add secret

---
+++ terraform/cloudrun.tf
@@ -0,0 +1,34 @@
+resource "google_cloud_run_service" "service" {
+  name     = "library-mawar"
+  location = var.region
+
+  template {
+    spec {
+      containers {
+        image = "asia-southeast2-docker.pkg.dev/${var.project}/library/library:library@sha256:471dbafe1f91f88fb9bb35a4be2a134469b1ce555c7c5ff53086f630b5951d80"
+        ports {
+            container_port = 80
+        }
+      }
+    }
+  }
+
+  traffic {
+    percent = 100
+    latest_revision = true
+    }
+
+  depends_on = [google_project_service.run]
+}
+
+resource "google_project_service" "run" {
+  service = "run.googleapis.com"
+}
+
+#parametersecurity
+resource "google_cloud_run_service_iam_member" "noauth" {
+     service = google_cloud_run_service.service.name
+     location = google_cloud_run_service.service.location
+     role = "roles/run.invoker"
+     member = "allUsers"
+     }
