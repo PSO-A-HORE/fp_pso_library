1d5b6f5dfbba94917c1fadf53b19073bdd67d25e
IndriyaniAlif <147221972+IndriyaniAlif@users.noreply.github.com>
2024-12-11 18:00:06 +0700

    add cloud sql connection (#22)

--- terraform/cloudrun.tf
+++ terraform/cloudrun.tf
@@ -11,8 +11,15 @@ resource "google_cloud_run_service" "service" {
         }
       }
     }
+    metadata {
+        annotations = {
+          "run.googleapis.com/cloudsql-instances" = google_sql_database_instance.db_instance.connection_name
+          "run.googleapis.com/client-name"        = "library_db"
+        }
+     }
   }
-
+  autogenerate_revision_name = true
+  
   traffic {
     percent         = 100
     latest_revision = true
